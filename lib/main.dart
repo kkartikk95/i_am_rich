9b95731dfec253a5df741b970c091306115fdc9f
Kartikeyan Venkatasubramanian <87036022+kkartikk95@users.noreply.github.com>
2023-08-16 20:13:22 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('I am Rich'),
+          backgroundColor: Colors.teal,
+        ),
+      ),
+    ),
+  );
+}
